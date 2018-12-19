698b5effcce3ab4ca2a0b98bfabfccf804f53f2f
Olivier Bossel <olivier.bossel@gmail.com>
2018-12-19 11:37:54 +0100

    preparing release 1.0.0

--- dist/index.js
+++ dist/index.js
@@ -3,89 +3,96 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = exports.WebWorker = exports.registerWorker = void 0;
+exports.expose = expose;
+exports.default = exports.registerWorker = void 0;
 
 var _detectBrowser = require("detect-browser");
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 var browser = (0, _detectBrowser.detect)();
 var workers = [];
 var storeRef;
+/**
+ * Register a worker
+ * @param    {Worker,Function}    WorkerInstance    A worker instance loaded through `worker-loader`, or the result of the `expose` function in case of NodeJS use
+ * @return    {Function}    The unregister mecanisms function
+ * @author    Olivier Bossel <olivier.bossel@gmail.com> (https://olivierbossel.com)
+ */
 
 var registerWorker = function registerWorker(WorkerInstance) {
   var newWorker; // register only if we are in a browser environment
 
   if (browser) {
     // create the new worker instance
-    newWorker = new WorkerInstance(); // add the new worker inside the workers stack
-
-    workers.push(newWorker); // listen for new messages from the worker
+    newWorker = new WorkerInstance(); // listen for new messages from the worker
 
     newWorker.addEventListener('message', function (e) {
       // dispatch a the new event in the system
       storeRef.dispatch(e.data);
     });
-  } // return the terminate function
+  } else {
+    newWorker = WorkerInstance();
+  } // add the new worker inside the workers stack
 
+
+  workers.push(newWorker); // return the terminate function
 
   return function () {
     if (newWorker) {
-      newWorker.terminate();
+      if (browser) newWorker.terminate();
       workers.splice(workers.indexOf(newWorker), 1);
     }
   };
-}; // base class to extends inside the worker
+};
+/**
+ * Expose our "API" to the web worker middleware
+ * @param    {Object}    tasks    The tasks object on where we have our actions defined
+ * @param    {Object}    self    The "self" variable of the worker context
+ * @return    {Function}    A function that expose a fake api for NodeJS compatibility
+ * @author    Olivier Bossel <olivier.bossel@gmail.com> (https://olivierbossel.com)
+ */
 
 
 exports.registerWorker = registerWorker;
 
-var WebWorker =
-/*#__PURE__*/
-function () {
-  function WebWorker(self) {
-    var _this = this;
+function expose(tasks, self) {
+  // in case we are in the browser
+  if (browser) {
+    tasks._dispatch = function (action) {
+      self.postMessage(action);
+    }; // listen for messages from the app
 
-    _classCallCheck(this, WebWorker);
-
-    this.self = self; // listen for messages from the app
 
     self.addEventListener('message', function (e) {
       // call the method that has the name of the action type
-      if (_this[e.data.type]) {
-        _this[e.data.type]({
+      if (tasks[e.data.type]) {
+        tasks[e.data.type]({
           action: e.data,
-          dispatch: _this._dispatch.bind(_this)
+          dispatch: tasks._dispatch.bind(tasks)
         });
       }
     });
-  }
-  /**
-   * Internal dispatch method
-   * @param    {Object}    action    The action to dispatch
-   */
+  } // return a function that expose a fake postMessage API for NodeJS
 
 
-  _createClass(WebWorker, [{
-    key: "_dispatch",
-    value: function _dispatch(action) {
-      this.self.postMessage(action);
-    }
-  }]);
-
-  return WebWorker;
-}();
+  return function () {
+    return {
+      postMessage: function postMessage(data) {
+        if (!tasks[data.type]) return;
+        tasks[data.type]({
+          action: data,
+          dispatch: function dispatch(action) {
+            storeRef.dispatch(action);
+          }
+        });
+      }
+    };
+  };
+}
 /**
  * Web worker middleware that abstract the use of web workers
  * to handle the actions dispatched in the redux system
  */
 
-
-exports.WebWorker = WebWorker;
 
 var webworkerMiddleware = function webworkerMiddleware(store) {
   // save store in a ref to share it with other functions
--- src/index.js
+++ src/index.js
@@ -4,52 +4,78 @@ const browser = detect()
 const workers = []
 let storeRef
 
-export const registerWorker = (WorkerInstance) => {
+/**
+ * Register a worker
+ * @param    {Worker,Function}    WorkerInstance    A worker instance loaded through `worker-loader`, or the result of the `expose` function in case of NodeJS use
+ * @return    {Function}    The unregister mecanisms function
+ * @author    Olivier Bossel <olivier.bossel@gmail.com> (https://olivierbossel.com)
+ */
+export const registerWorker = WorkerInstance => {
   let newWorker
   // register only if we are in a browser environment
   if (browser) {
     // create the new worker instance
     newWorker = new WorkerInstance()
-    // add the new worker inside the workers stack
-    workers.push(newWorker)
     // listen for new messages from the worker
     newWorker.addEventListener('message', e => {
       // dispatch a the new event in the system
       storeRef.dispatch(e.data)
     })
+  } else {
+    newWorker = WorkerInstance()
   }
+
+  // add the new worker inside the workers stack
+  workers.push(newWorker)
+
   // return the terminate function
   return () => {
     if (newWorker) {
-      newWorker.terminate()
+      if (browser) newWorker.terminate()
       workers.splice(workers.indexOf(newWorker), 1)
     }
   }
 }
 
-// base class to extends inside the worker
-export class WebWorker {
-  constructor(self) {
-    this.self = self
+/**
+ * Expose our "API" to the web worker middleware
+ * @param    {Object}    tasks    The tasks object on where we have our actions defined
+ * @param    {Object}    self    The "self" variable of the worker context
+ * @return    {Function}    A function that expose a fake api for NodeJS compatibility
+ * @author    Olivier Bossel <olivier.bossel@gmail.com> (https://olivierbossel.com)
+ */
+export function expose(tasks, self) {
+
+  // in case we are in the browser
+  if (browser) {
+    tasks._dispatch = (action) => {
+      self.postMessage(action)
+    }
+
     // listen for messages from the app
     self.addEventListener('message', e => {
       // call the method that has the name of the action type
-      if (this[e.data.type]) {
-        this[e.data.type]({
+      if (tasks[e.data.type]) {
+        tasks[e.data.type]({
           action: e.data,
-          dispatch: this._dispatch.bind(this)
+          dispatch: tasks._dispatch.bind(tasks)
         })
       }
     })
   }
 
-  /**
-   * Internal dispatch method
-   * @param    {Object}    action    The action to dispatch
-   */
-  _dispatch(action) {
-    this.self.postMessage(action)
-  }
+  // return a function that expose a fake postMessage API for NodeJS
+  return () => ({
+    postMessage(data) {
+      if (!tasks[data.type]) return
+      tasks[data.type]({
+        action: data,
+        dispatch: (action) => {
+          storeRef.dispatch(action)
+        }
+      })
+    }
+  })
 }
 
 /**
@@ -57,16 +83,14 @@ export class WebWorker {
  * to handle the actions dispatched in the redux system
  */
 const webworkerMiddleware = store => {
-
   // save store in a ref to share it with other functions
   storeRef = store
 
   // return the rest of the curried functions
   return next => action => {
-
     // on each action dispatched in the redux system,
     // pass it to each registered worker(s)
-    workers.forEach((worker) => {
+    workers.forEach(worker => {
       worker.postMessage(action)
     })
     next(action)
